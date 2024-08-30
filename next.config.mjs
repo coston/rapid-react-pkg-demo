f3aa133c9902aa1814b0505a0c4ee9dcf772a232
Coston Perkins <costonperkins@me.com>
2024-08-30 16:00:12 -0500

    refactor: extend webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["next/babel", "@babel/preset-typescript"],
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
