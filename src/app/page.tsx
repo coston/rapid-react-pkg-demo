e3aeaa8b3ef726dc22ed744898d41350268509b8
Coston Perkins <costonperkins@me.com>
2024-09-02 14:27:03 -0500

    refactor: remove dev handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,14 @@ const readReadmeContent = (filePath: string): string => {
   return "README.md file not found.";
 };
 
-const copyReadmeToPublic = (sourcePath: string, destinationPath: string) => {
-  if (fs.existsSync(sourcePath)) {
-    fs.copyFileSync(sourcePath, destinationPath);
-    console.log("README.md copied to public directory.");
-  }
-};
-
 async function HomePage() {
-  const readmePath =
-    process.env.README_PATH || path.resolve(process.cwd(), "README.md");
   const publicReadmePath = path.resolve(
     process.cwd(),
     "src/public",
     "README.md"
   );
 
-  const readmeContent = readReadmeContent(readmePath);
-
-  // duplicated copy README.md to public directory to keep it updated in development
-  copyReadmeToPublic(readmePath, publicReadmePath);
+  const readmeContent = readReadmeContent(publicReadmePath);
 
   return <Main content={readmeContent} />;
 }
