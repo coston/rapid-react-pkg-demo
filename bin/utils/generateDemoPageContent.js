2763e93fb9724267e2e3a98844b784d81c91b254
Coston Perkins <costonperkins@me.com>
2024-09-04 09:26:19 -0500

    feat: simplify processing

--- bin/utils/generateDemoPageContent.js
+++ bin/utils/generateDemoPageContent.js
@@ -1,22 +1,17 @@
-const fs = require("fs");
 const path = require("path");
 const writeFile = require("./writeFile");
 const generateContent = require("./generateContent");
-const processConfigFile = require("./processConfigFile");
-const resolveConfigPaths = require("./resolveConfigPaths");
+
 const generateManifest = require("./generateManifest");
-const readFileContent = (filePath) => fs.readFileSync(filePath, "utf-8");
 
 function generateDemoPageContent(configPath) {
   const mainPath = path.join(__dirname, "../../app/components/Main.tsx");
   const manifestPath = path.join(__dirname, "../../app/manifest.ts");
 
-  const configContent = readFileContent(configPath);
-  const configPaths = resolveConfigPaths(configContent, configPath);
-  const configObject = processConfigFile(configContent);
+  const absoluteConfigPath = path.resolve(process.cwd(), configPath);
 
-  const content = generateContent(configObject, configPaths);
-  const manifest = generateManifest(configObject);
+  const content = generateContent(absoluteConfigPath);
+  const manifest = generateManifest(absoluteConfigPath);
 
   writeFile(mainPath, content);
   writeFile(manifestPath, manifest);
--- bin/utils/generateManifest.js
+++ bin/utils/generateManifest.js
@@ -1,15 +1,16 @@
-function generateManifest(config) {
+function generateManifest(configPath) {
   return `import { MetadataRoute } from "next"
+import config from "${configPath}";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "${config.packageName} docs",
-    short_name: "${config.packageName}",
-    description: "${config.description}",
+    name: config.packageName + " docs",
+    short_name: config.packageName,
+    description: config.description,
     start_url: "/",
     display: "standalone",
-    background_color: "${config.color}",
-    theme_color: "${config.color}",
+    background_color: config.color,
+    theme_color: config.color,
     icons: [
       {
         src: "/favicon.ico",
