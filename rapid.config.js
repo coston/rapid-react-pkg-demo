ce8e82be93d22bb37a4047411122a19ef08dab46
Coston Perkins <costonperkins@me.com>
2024-09-04 09:31:35 -0500

    style: formatting

--- rapid.config.js
+++ rapid.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const config = {
   color: "#bfe0ea",
   packageName: "rapid-react-pkg-demo",
   description: "A CLI tool for quickly creating react package demos",
   icon: "🌊",
 };
+
+export default config;
